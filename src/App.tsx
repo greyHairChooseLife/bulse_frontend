d31c0ebff5b8491353dc746e6b19d70f48cb90c2
sy <greyhairchooselife@gmail.com>
2022-05-28 23:04:37 +0900

    react-typescript 기본 설정

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+			fck
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
