a513551c1d24ba2f68202a968d8996ff57c503c9
sy <greyhairchooselife@gmail.com>
2022-05-29 00:11:01 +0900

    3가지 mode 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import 'App.css';
+
+type mainMode = 'user' | 'project' | 'arena' | null;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-			fck
-        </p>
-      </header>
-    </div>
-  );
+
+	const [ mainMode, setMainMode ] = useState<mainMode>(null);
+
+	let article = null;
+	switch (mainMode){
+		case 'user':
+			article = 'userComponent';
+			break;
+		case 'project':
+			article = 'projectComponent';
+			break;
+		case 'arena':
+			article = 'arenaComponent';
+			break;
+		default :
+			console.log('mainMode :', mainMode);
+	}
+
+	return (
+		<div className="App">
+			<button className="btnUser" onClick={() => {
+				setMainMode('user');
+			}}>user</button>
+			<button className="btnProject" onClick={() => {
+				setMainMode('project');
+			}}>project</button>
+			<button className="btnArena" onClick={() => {
+				setMainMode('arena');
+			}}>arena</button>
+
+			{ article }
+		</div>
+	);
 }
 
 export default App;
