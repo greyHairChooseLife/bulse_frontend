53bdca3774ade32a74afaad614238ed5300aa8b9
sy <greyhairchooselife@gmail.com>
2022-05-31 21:09:21 +0900

    #1 identity ErrMsg 구체화, 적절한 형식으로 바꾸는 기능을 JSX의 blur 이벤트 속성에 넣어서 유사 동기식 처리, 제출 버튼 클릭 시 모든 유효성을 만족하는지 확인, identity Input 유효성 확인 되는 순간과 project Input 확인되는 순간 axios.post보낼 객체를 setter함수로 최종 업데이트

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -7,6 +7,9 @@ const api = axios.create({
 	baseURL: `http://${process.env.REACT_APP_API_SERVER_HOST}:${process.env.REACT_APP_API_SERVER_PORT}`,
 })
 console.log('axios baseURL : ', `http://${process.env.REACT_APP_API_SERVER_HOST}:${process.env.REACT_APP_API_SERVER_PORT}`);
+
+// TYPE & INTERFACE
+type postNewProjectType ={registerDate: Date, projectDate: Date } & IIdentity & INewProject;
 
 interface IIdentity {
 	mobileNumber: number | ''
@@ -49,6 +52,22 @@ export const Project = () => {
 	const [ bankHostErrMsg, setBankHostErrMsg ] = useState<string>('어떤 은행인지 입력 해 주세요.');
 	const [ bankHolderNameErrMsg, setBankHolderNameErrMsg ] = useState<string>('계좌 소유자를 입력 해 주세요.');
 
+	const initialPostBag: postNewProjectType = {
+		registerDate: new Date(),
+		mobileNumber: '',
+		name: '',
+		entered: false,
+		projectDate: new Date(),
+		projectTime: 0,
+		projectHour: 0,
+		projectSubject: '',
+		projectKeyword: '',
+		projectDescription: '',
+		bankAccount: 0,
+		bankHost: '',
+		bankHolderName: '',
+	}
+	const [ postBag, setPostBag ] = useState<postNewProjectType>(initialPostBag);
 
 	// About IDENTITY
 	// 타이핑 할 때마다 setter함수 실행
@@ -59,10 +78,11 @@ export const Project = () => {
 			[name]: value,
 		});
 		
+	// 이름, 전화번호 유효성 확인
 		switch(name){
 			case 'name':
 				if(value.trim() === '') setNameErrMsg('이름을 입력 해 주세요.');
-				else setNameErrMsg('good');
+				else setNameErrMsg('');
 				break;
 			case 'mobileNumber':
 				let haveNaN = false;
@@ -71,40 +91,17 @@ export const Project = () => {
 				})
 				if(haveNaN !== false) setMobileNumberErrMsg('숫자만 입력 해 주세요.');
 				if(value.length !== 11) setMobileNumberErrMsg('11자리 전화번호를 정확히 입력해 주세요.');
-				if(haveNaN === false && value.length === 11) setMobileNumberErrMsg('good');
+				if(haveNaN === false && value.length === 11) setMobileNumberErrMsg('');
 			default:
 				console.log('something wrong at Project.tsx');
 		}
 	};
 
-	// 이름, 전화번호 유효성 확인
-	const checkValideIdentity = (name: string, mobileNumber: number | ''): boolean => {
-		if(String(mobileNumber).length !== 11){
-			alert('11자리 전화번호를 정확히 입력해 주세요.');
-			return false;
-		}
-		let haveNaN = false;
-		String(mobileNumber).split('').forEach((ele) => {
-			console.log(parseInt(ele));
-			if(isNaN(parseInt(ele))) haveNaN = true;
-		})
-		if(haveNaN !== false){
-			alert(' 넣어 주세요.');
-			return false;
-		}
-		if(name.trim() === ''){
-			alert('이름을 입력 해 주세요.')
-			return false;
-		}
-
-		return true;
-	};
-
-	// 입력된 이름, 전화번호로 등록된 projectList 받아오기
-	const getProjectList = async () => {
-		const response = await api.get(`/project/${identity.mobileNumber}/${identity.name}`);
-		setProjectList(response.data);
-	};
+	//	모든 요소가 유효한지 확인
+	const onClickIsFineIdentity = (): boolean => {
+		if(mobileNumberErrMsg + nameErrMsg === '') return true;
+		else return false;
+	}
 
 	const identityInput = 
 		<div>
@@ -114,16 +111,34 @@ export const Project = () => {
 			<input type="text" placeholder="mobile" onChange={onChangeIdentity} value={identity.mobileNumber} name="mobileNumber"></input>
 			<label>{mobileNumberErrMsg}</label>
 			<button onClick={() => {
-//				if(checkValideIdentity(identity.name, identity.mobileNumber)){
-//					getProjectList();
-//					setIdentity({
-//						...identity,
-//						entered: true
-//					});
-//					console.log('identity validation success');
-//				}
-			}}>확인</button>
+				if(onClickIsFineIdentity()){
+					getProjectList();
+					setIdentity({
+						...identity,
+						entered: true
+					});
+					setPostBag({
+						...postBag,
+						name: identity.name,
+						mobileNumber: identity.mobileNumber,
+						entered: true
+					})
+				}
+				}} onSubmit={(e)=>e.preventDefault()}>확인</button>
 		</div>;
+
+	// 입력된 이름, 전화번호로 등록된 projectList 받아오기
+	const getProjectList = async () => {
+		const response = await api.get(`/project/${identity.mobileNumber}/${identity.name}`);
+		setProjectList(response.data);
+	};
+
+	//	About newPROJECT
+	//	모든 요소가 유효한지 확인
+	const onClickIsFineNewProject = (): boolean => {
+		if(subjectErrMsg + descriptionErrMsg + bankAccountErrMsg + bankHostErrMsg + bankHolderNameErrMsg === '') return true;
+		else return false;
+	}
 
 	// 타이핑 할 때마다 setter함수 실행
 	const onChangeProject = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
@@ -167,88 +182,79 @@ export const Project = () => {
 		}
 	};
 
-//			<button onClick={async () => {
-//				const form: {sanitizing: ISanitizeNewProject, posting: IPostNewProject} = {
-//					sanitizing: {
-//						keyword: newProject.projectKeyword,
-//					},
-//					posting: {
-//						readyToPost: false,
-//						identity: identity,
-//						newProject: newProject,
-//						newProjectDate: newProjectDate
-//					}
-//				}
-//				console.log('validaty confirmed');
-//				sanitizeProjectInput(form.sanitizing).then(() => console.log(newProject))
-//			}}>신청완료</button>
-
-	//
 	// 적절한 형식으로 변환
-	interface ISanitizeNewProject {
-		keyword?: string
-	}
-	const sanitizeProjectInput = async (form: ISanitizeNewProject) => {
-		const {keyword} = form;
+	const sanitizeProjectInput = (keyword?: string) => {
 		let sanitized;
 		if(keyword !== undefined){
 			sanitized = keyword.split(',').map(ele => {return ele.trim()}).join(', ');
+			setNewProject({
+				...newProject,
+				projectKeyword: sanitized
+			});
 		}
-		setNewProject({
-			...newProject,
-			projectKeyword: sanitized
-		});
 	}
 	
-	// 
-	interface IPostNewProject {
-		readyToPost: boolean
-		identity: IIdentity
-		newProject: INewProject
-		newProjectDate: Date
-	}
-	// 입력된 이름, 전화번호로 등록된 projectList 받아오기
-	// const postNewProject = () => { api.post() }
-
 	const projectInput =
 		<div>
-			<h2>about Project</h2>
-			<Calendar defaultValue={new Date()} minDate={new Date()} onChange={setNewProjectDate} value={newProjectDate}></Calendar>
-			<select size={1} onChange={onChangeProject} name="projectTime">
-				<option value="14">오후 2시</option>
-				<option value="15">오후 3시</option>
-				<option value="16">오후 4시</option>
-				<option value="17">오후 5시</option>
-				<option value="18">오후 6시</option>
-				<option value="19">오후 7시</option>
-				<option value="20">오후 8시</option>
-				<option value="21">오후 9시</option>
-				<option value="22">오후 10시</option>
-			</select>
-			<select size={1} onChange={onChangeProject} name="projectHour">
-				<option value="1">1시간</option>
-				<option value="2">2시간</option>
-			</select>
-			<input type="text" placeholder="projectSubject" onChange={onChangeProject} name="projectSubject"></input>
-			<label>{subjectErrMsg}</label>
+			<form onSubmit={e => {
+				e.preventDefault();
+				console.log(postBag);
+				//api.post('2');
+				}}>
+				<h2>about Project</h2>
+				<Calendar defaultValue={new Date()} minDate={new Date()} onChange={setNewProjectDate} value={newProjectDate}></Calendar>
+				<select size={1} onChange={onChangeProject} name="projectTime">
+					<option value="14">오후 2시</option>
+					<option value="15">오후 3시</option>
+					<option value="16">오후 4시</option>
+					<option value="17">오후 5시</option>
+					<option value="18">오후 6시</option>
+					<option value="19">오후 7시</option>
+					<option value="20">오후 8시</option>
+					<option value="21">오후 9시</option>
+					<option value="22">오후 10시</option>
+				</select>
+				<select size={1} onChange={onChangeProject} name="projectHour">
+					<option value="1">1시간</option>
+					<option value="2">2시간</option>
+				</select>
+				<input type="text" placeholder="projectSubject" onChange={onChangeProject} name="projectSubject"></input>
+				<label>{subjectErrMsg}</label>
 
-			<input type="text" placeholder="projectKeyword" onChange={onChangeProject} name="projectKeyword"></input>
+				<input type="text" placeholder="projectKeyword" onChange={onChangeProject} onBlur={()=>{sanitizeProjectInput(newProject.projectKeyword)}} name="projectKeyword"></input>
 
-			<textarea placeholder="projectDescription" onChange={onChangeProject} name="projectDescription"></textarea>
-			<label>{descriptionErrMsg}</label>
+				<textarea placeholder="projectDescription" onChange={onChangeProject} name="projectDescription"></textarea>
+				<label>{descriptionErrMsg}</label>
 
-			<h2>about Bank</h2>
-			<input type="text" placeholder="bankAccount" onChange={onChangeProject} name="bankAccount"></input>
-			<label>{bankAccountErrMsg}</label>
+				<h2>about Bank</h2>
+				<input type="text" placeholder="bankAccount" onChange={onChangeProject} name="bankAccount"></input>
+				<label>{bankAccountErrMsg}</label>
 
-			<input type="text" placeholder="bankHost" onChange={onChangeProject} name="bankHost"></input>
-			<label>{bankHostErrMsg}</label>
+				<input type="text" placeholder="bankHost" onChange={onChangeProject} name="bankHost"></input>
+				<label>{bankHostErrMsg}</label>
 
-			<input type="text" placeholder="bankHolderName" onChange={onChangeProject} name="bankHolderName"></input>
-			<label>{bankHolderNameErrMsg}</label>
+				<input type="text" placeholder="bankHolderName" onChange={onChangeProject} name="bankHolderName"></input>
+				<label>{bankHolderNameErrMsg}</label>
 
-			<button onClick={() => {
-			}}>신청완료</button>
+				<button type="submit" onClick={() => {
+					if(!onClickIsFineNewProject()){
+						alert('잘 입력 해줘요!!');
+					}else{	
+						setPostBag({
+							...postBag,
+							projectDate: newProjectDate,
+							projectTime: newProject.projectTime,
+							projectHour: newProject.projectHour,
+							projectSubject: newProject.projectSubject,
+							projectKeyword: newProject.projectKeyword,
+							projectDescription: newProject.projectDescription,
+							bankAccount: newProject.bankAccount,
+							bankHost: newProject.bankHost,
+							bankHolderName: newProject.bankHolderName,
+						})
+					}
+				}}>신청완료</button>
+			</form>
 		</div>;
 
 	return (
